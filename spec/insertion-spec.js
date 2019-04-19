429a12741ddabcff8178578888bddff593ccfead
Benjamin Gray <bengray178@gmail.com>
2019-04-20 00:26:19 +1000

    start testing snippet body text

--- spec/insertion-spec.js
+++ spec/insertion-spec.js
@@ -1,9 +1,128 @@
 const Insertion = require('../lib/insertion')
-const {Range} = require('atom')
+const {Range, TextEditor} = require('atom')
 
 const range = new Range(0, 0)
 
+const Snippets = require('../lib/snippets')
+
 describe('Insertion', () => {
+  let editor
+  let editorElement
+
+  beforeEach(() => {
+    spyOn(Snippets, 'loadAll')
+    spyOn(Snippets, 'getUserSnippetsPath').andReturn('')
+
+    waitsForPromise(() => atom.workspace.open())
+    waitsForPromise(() => atom.packages.activatePackage('snippets'))
+
+    runs(() => {
+      editor = atom.workspace.getActiveTextEditor()
+      editorElement = atom.views.getView(editor)
+    })
+  })
+
+  function resolve (snippet) {
+    Snippets.add(__filename, {
+      '*': {
+        'a': {
+          prefix: 'a',
+          body: snippet
+        }
+      }
+    })
+
+    editor.setText('a')
+    editor.setCursorBufferPosition([0, 1])
+    atom.commands.dispatch(editorElement, 'snippets:expand')
+    return editor.getText()
+  }
+
+  it('resolves a plain snippet', () => {
+    expect(resolve('${} $ n $}1} ${/upcase/} \n world ${||}'))
+      .toEqual('${} $ n $}1} ${/upcase/} \n world ${||}')
+  })
+
+  it('resolves a snippet with tabstops', () => {
+    expect(resolve('hello$1world${2}')).toEqual('helloworld')
+  })
+
+  it('resolves snippets with placeholders', () => {
+    expect(resolve('${1:hello} world')).toEqual('hello world')
+    expect(resolve('${1:one${2:tw${3:othre}e}}')).toEqual('onetwothree')
+  })
+
+  it('uses the first choice as a placeholder', () => {
+    expect(resolve('${1|one,two,three|}')).toEqual('one')
+  })
+
+  describe('when resolving variables', () => {
+    it('resolves base variables', () => {
+      expect(resolve('$TM_LINE_INDEX')).toEqual('0')
+      expect(resolve('$TM_LINE_NUMBER')).toEqual('1')
+      expect(/\d{4,}/.test(resolve('$CURRENT_YEAR'))).toEqual(true)
+
+      atom.clipboard.write('foo')
+      expect(resolve('$CLIPBOARD')).toEqual('foo')
+    })
+
+    it('uses unknown variables as placeholders', () => {
+      expect(resolve('$GaRBag3')).toEqual('GaRBag3')
+    })
+
+    it('allows more resolvers to be provided', () => {
+      Snippets.consumeResolver({
+        variableResolvers: {
+          'EXTENDED': () => 'calculated resolution',
+          'POSITION': ({row}) => `${row}`
+        }
+      })
+
+      expect(resolve('$EXTENDED')).toEqual('calculated resolution')
+      expect(resolve('foo\n$POSITION')).toEqual('foo\n1')
+    })
+
+    it('allows provided resolvers to override builtins', () => {
+      expect(resolve('$TM_LINE_INDEX')).toEqual('0')
+      Snippets.consumeResolver({
+        variableResolvers: {
+          'TM_LINE_INDEX': () => 'umbrella'
+        }
+      })
+      expect(resolve('$TM_LINE_INDEX')).toEqual('umbrella')
+    })
+  })
+
+  describe('when resolving transforms', () => {
+    beforeEach(() => {
+      Snippets.consumeResolver({
+        variableResolvers: {
+          'A': () => 'hello world',
+          'B': () => 'foo\nbar\nbaz',
+          'C': () => '😄foo',
+          'D': () => 'baz foo'
+        }
+      })
+    })
+
+    it('respects the provided flags', () => {
+      expect(resolve('${A/.//}')).toEqual('ello world')
+      expect(resolve('${A/.//g}')).toEqual('')
+
+      expect(resolve('${A/HELLO//}')).toEqual('hello world')
+      expect(resolve('${A/HELLO//i}')).toEqual(' world')
+
+      expect(resolve('${B/^ba(.)$/$1/}')).toEqual('foo\nbar\nbaz')
+      expect(resolve('${B/^ba(.)$/$1/m}')).toEqual('foo\nr\nbaz')
+
+      expect(resolve('${C/^.foo$/bar/}')).toEqual('😄foo') // without /u, the emoji is seen as two characters
+      expect(resolve('${C/^.foo$/bar/u}')).toEqual('bar')
+
+      expect(resolve('${D/foo/bar/}')).toEqual('baz bar')
+      expect(resolve('${D/foo/bar/y}')).toEqual('baz foo') // with /y, the search is only from index 0 and fails
+    })
+  })
+
   it('returns what it was given when it has no substitution', () => {
     let insertion = new Insertion({
       range,
