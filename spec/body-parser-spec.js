e4822b6fb6737b6b4979f95d1cb939ca5f0542f1
Benjamin Gray <bengray178@gmail.com>
2020-07-16 20:46:21 +1000

    more tests

--- spec/body-parser-spec.js
+++ spec/body-parser-spec.js
@@ -5,7 +5,7 @@ describe("Snippet Body Parser", () => {
     expect(SnippetParser.parse(input)).toEqual(tree);
   }
 
-  describe("tab stops", () => {
+  describe("When parsing tab stops", () => {
     it("parses simple tab stops", () => {
       expectMatch("hello$1world${2}", [
         "hello", { index: 1, content: [] }, "world", { index: 2, content: [] },
@@ -21,6 +21,7 @@ describe("Snippet Body Parser", () => {
     });
 
     it("only allows non-negative integer stop numbers", () => {
+      expectMatch("$0", [{ index: 0, content: [] }]);
       expectMatch("$99999", [{ index: 99999, content: [] }]);
       expectMatch("$-1", ["$-1"]);
       expectMatch("${-1}", ["${-1}"]);
@@ -45,8 +46,20 @@ describe("Snippet Body Parser", () => {
       it("allows escaping '}' in placeholders", () => {
         expectMatch("${1:\\}}", [{ index: 1, content: ["}"] }]);
       });
+
+      it("allows '$' in placeholders", () => {
+        expectMatch("${1:$}", [
+          {
+            index: 1,
+            content: [
+              "$"
+            ]
+          }
+        ]);
+      });
     });
 
+    // See the transformations section for more thorough testing
     describe("with transformations", () => {
       it("parses simple transformations", () => {
         expectMatch("${1/foo/bar/}", [
@@ -61,35 +74,10 @@ describe("Snippet Body Parser", () => {
           }
         ]);
       });
-
-      it("applies flags to the find regex", () => {
-        expectMatch("${1/foo/bar/gimsuy}", [
-          {
-            index: 1,
-            transformation: {
-              find: /foo/gimsuy,
-              replace: [
-                "bar"
-              ]
-            }
-          }
-        ]);
-      });
-
-      it("does not parse invalid regex as transformations", () => {
-        expectMatch("${1/foo/bar/a}", ["${1/foo/bar/a}"]); // invalid flag
-        expectMatch("${1/fo)o$1/$bar/}", [
-          "${1/fo)o",
-          { index: 1, content: [] },
-          "/",
-          { variable: "bar" },
-          "/}"
-        ]);
-      });
     });
   });
 
-  describe("variables", () => {
+  describe("When parsing variables", () => {
     it("parses simple variables", () => {
       expectMatch("hello$foo2__bar&baz${abc}d", [
         "hello",
@@ -141,6 +129,7 @@ describe("Snippet Body Parser", () => {
       });
     });
 
+    // See the transformations section for more thorough testing
     describe("with transformations", () => {
       it("parses simple transformations", () => {
         expectMatch("${var/foo/bar/}", [
@@ -158,13 +147,13 @@ describe("Snippet Body Parser", () => {
     });
   });
 
-  describe("choices", () => {
-    it("parses choices", () => {
+  describe("When parsing choices", () => {
+    it("parses simple choices", () => {
       expectMatch("${1|a,b,c|}", [{ index: 1, choices: ["a", "b", "c"] }]);
     });
 
-    it("skips empty choices", () => {
-      expectMatch("${1||}", ["${1||}"]);
+    it("parses empty choices", () => {
+      expectMatch("${1||}", [{ index: 1, choices: [] }]);
     });
 
     it("skips escaped choices", () => {
@@ -190,7 +179,38 @@ describe("Snippet Body Parser", () => {
     });
   });
 
-  describe("escaped characters", () => {
+  describe("When parsing transformations", () => {
+    it("allows an empty transformation", () => {
+      expectMatch("${1///}", [{ index: 1, transformation: { find: new RegExp(""), replace: [] } }]);
+    });
+
+    it("applies flags to the find regex", () => {
+      expectMatch("${1/foo/bar/gimsuy}", [
+        {
+          index: 1,
+          transformation: {
+            find: /foo/gimsuy,
+            replace: [
+              "bar"
+            ]
+          }
+        }
+      ]);
+    });
+
+    it("does not parse invalid regex as transformations", () => {
+      expectMatch("${1/foo/bar/a}", ["${1/foo/bar/a}"]); // invalid flag
+      expectMatch("${1/fo)o$1/$bar/}", [
+        "${1/fo)o",
+        { index: 1, content: [] },
+        "/",
+        { variable: "bar" },
+        "/}"
+      ]);
+    });
+  });
+
+  describe("When parsing escaped characters", () => {
     const escapeTest = "\\$ \\\\ \\} \\% \\* \\, \\| \\{ \\n \\r \\:";
 
     const escapeResolveTop = "$ \\ } \\% \\* \\, \\| \\{ \\n \\r \\:";
@@ -222,196 +242,208 @@ describe("Snippet Body Parser", () => {
     });
   });
 
-  it("breaks a snippet body into lines, with each line containing tab stops at the appropriate position", () => {
-    expectMatch("the quick brown $1fox ${2:jumped ${3:over}\n}the ${4:lazy} dog", [
-      "the quick brown ",
-      { index: 1, content: [] },
-      "fox ",
-      {
-        index: 2,
-        content: [
-          "jumped ",
-          { index: 3, content: ["over"] },
-          "\n"
-        ],
-      },
-      "the ",
-      { index: 4, content: ["lazy"] },
-      " dog"
-    ]);
+  describe("When a potential non-text parse fails", () => {
+    it("accepts the first character as text and resumes", () => {
+      expectMatch("${1:${2}${3}", [
+        "${1:",
+        { index: 2, content: [] },
+        { index: 3, content: [] }
+      ]);
+    });
   });
 
-  it("supports interpolated variables in placeholder text", () => {
-    expectMatch("module ${1:ActiveRecord::${TM_FILENAME/(?:\\A|_)([A-Za-z0-9]+)(?:\\.rb)?/(?2::\\u$1)/g}}", [
-      "module ",
-      {
-        index: 1,
-        content: [
-          "ActiveRecord::",
-          {
-            variable: "TM_FILENAME",
-            transformation: {
-              find: /(?:\A|_)([A-Za-z0-9]+)(?:\.rb)?/g,
-              replace: [
-                "(?2::",
-                {
-                  modifier: "u",
-                },
-                {
-                  backreference: 1,
-                },
-                ")",
-              ]
+  describe("examples", () => {
+    it("breaks a snippet body into lines, with each line containing tab stops at the appropriate position", () => {
+      expectMatch("the quick brown $1fox ${2:jumped ${3:over}\n}the ${4:lazy} dog", [
+        "the quick brown ",
+        { index: 1, content: [] },
+        "fox ",
+        {
+          index: 2,
+          content: [
+            "jumped ",
+            { index: 3, content: ["over"] },
+            "\n"
+          ],
+        },
+        "the ",
+        { index: 4, content: ["lazy"] },
+        " dog"
+      ]);
+    });
+
+    it("supports interpolated variables in placeholder text", () => {
+      expectMatch("module ${1:ActiveRecord::${TM_FILENAME/(?:\\A|_)([A-Za-z0-9]+)(?:\\.rb)?/(?2::\\u$1)/g}}", [
+        "module ",
+        {
+          index: 1,
+          content: [
+            "ActiveRecord::",
+            {
+              variable: "TM_FILENAME",
+              transformation: {
+                find: /(?:\A|_)([A-Za-z0-9]+)(?:\.rb)?/g,
+                replace: [
+                  "(?2::",
+                  {
+                    modifier: "u",
+                  },
+                  {
+                    backreference: 1,
+                  },
+                  ")",
+                ]
+              }
             }
-          }
-        ],
-      }
-    ]);
-  });
-
-  it("parses a snippet with transformations", () => {
-    expectMatch("<${1:p}>$0</${1/f/F/}>", [
-      '<',
-      { index: 1, content: ['p'] },
-      '>',
-      { index: 0, content: [] },
-      '</',
-      { index: 1, transformation: { find: /f/, replace: ['F'] } },
-      '>',
-    ]);
-  });
-
-  it("parses a snippet with multiple tab stops with transformations", () => {
-    expectMatch("${1:placeholder} ${1/(.)/\\u$1/} $1 ${2:ANOTHER} ${2/^(.*)$/\\L$1/} $2", [
-      { index: 1, content: ['placeholder'] },
-      ' ',
-      {
-        index: 1,
-        transformation: {
-          find: /(.)/,
-          replace: [
-            { modifier: 'u' },
-            { backreference: 1 },
           ],
-        },
-      },
-      ' ',
-      { index: 1, content: [] },
-      ' ',
-      { index: 2, content: ['ANOTHER'] },
-      ' ',
-      {
-        index: 2,
-        transformation: {
-          find: /^(.*)$/,
-          replace: [
-            { modifier: 'L' },
-            { backreference: 1 },
-          ],
-        },
-      },
-      ' ',
-      { index: 2, content: [] },
-    ]);
-  });
+        }
+      ]);
+    });
 
-  it("parses a snippet with transformations and mirrors", () => {
-    expectMatch("${1:placeholder}\n${1/(.)/\\u$1/}\n$1", [
-      { index: 1, content: ['placeholder'] },
-      '\n',
-      {
-        index: 1,
-        transformation: {
-          find: /(.)/,
-          replace: [
-            { modifier: 'u' },
-            { backreference: 1 },
-          ],
+    it("parses a snippet with transformations", () => {
+      expectMatch("<${1:p}>$0</${1/f/F/}>", [
+        '<',
+        { index: 1, content: ['p'] },
+        '>',
+        { index: 0, content: [] },
+        '</',
+        { index: 1, transformation: { find: /f/, replace: ['F'] } },
+        '>',
+      ]);
+    });
+
+    it("parses a snippet with multiple tab stops with transformations", () => {
+      expectMatch("${1:placeholder} ${1/(.)/\\u$1/} $1 ${2:ANOTHER} ${2/^(.*)$/\\L$1/} $2", [
+        { index: 1, content: ['placeholder'] },
+        ' ',
+        {
+          index: 1,
+          transformation: {
+            find: /(.)/,
+            replace: [
+              { modifier: 'u' },
+              { backreference: 1 },
+            ],
+          },
         },
-      },
-      '\n',
-      { index: 1, content: [] },
-    ]);
-  });
-
-  it("parses a snippet with a format string and case-control flags", () => {
-    expectMatch("<${1:p}>$0</${1/(.)(.*)/\\u$1$2/}>", [
-      '<',
-      { index: 1, content: ['p'] },
-      '>',
-      { index: 0, content: [] },
-      '</',
-      {
-        index: 1,
-        transformation: {
-          find: /(.)(.*)/,
-          replace: [
-            { modifier: 'u' },
-            { backreference: 1 },
-            { backreference: 2 },
-          ],
+        ' ',
+        { index: 1, content: [] },
+        ' ',
+        { index: 2, content: ['ANOTHER'] },
+        ' ',
+        {
+          index: 2,
+          transformation: {
+            find: /^(.*)$/,
+            replace: [
+              { modifier: 'L' },
+              { backreference: 1 },
+            ],
+          },
         },
-      },
-      '>',
-    ]);
-  });
+        ' ',
+        { index: 2, content: [] },
+      ]);
+    });
 
-  it("parses a snippet with an escaped forward slash in a transform", () => {
-    expectMatch("<${1:p}>$0</${1/(.)\\/(.*)/\\u$1$2/}>", [
-      '<',
-      { index: 1, content: ['p'] },
-      '>',
-      { index: 0, content: [] },
-      '</',
-      {
-        index: 1,
-        transformation: {
-          find: /(.)\/(.*)/,
-          replace: [
-            { modifier: 'u' },
-            { backreference: 1 },
-            { backreference: 2 },
-          ],
+    it("parses a snippet with transformations and mirrors", () => {
+      expectMatch("${1:placeholder}\n${1/(.)/\\u$1/}\n$1", [
+        { index: 1, content: ['placeholder'] },
+        '\n',
+        {
+          index: 1,
+          transformation: {
+            find: /(.)/,
+            replace: [
+              { modifier: 'u' },
+              { backreference: 1 },
+            ],
+          },
         },
-      },
-      '>',
-    ]);
-  });
+        '\n',
+        { index: 1, content: [] },
+      ]);
+    });
 
-  it("parses a snippet with a placeholder that mirrors another tab stop's content", () => {
-    expectMatch("$4console.${3:log}('${2:$1}', $1);$0", [
-      { index: 4, content: [] },
-      'console.',
-      { index: 3, content: ['log'] },
-      '(\'',
-      {
-        index: 2, content: [
-          { index: 1, content: [] }
-        ]
-      },
-      '\', ',
-      { index: 1, content: [] },
-      ');',
-      { index: 0, content: [] }
-    ]);
-  });
+    it("parses a snippet with a format string and case-control flags", () => {
+      expectMatch("<${1:p}>$0</${1/(.)(.*)/\\u$1$2/}>", [
+        '<',
+        { index: 1, content: ['p'] },
+        '>',
+        { index: 0, content: [] },
+        '</',
+        {
+          index: 1,
+          transformation: {
+            find: /(.)(.*)/,
+            replace: [
+              { modifier: 'u' },
+              { backreference: 1 },
+              { backreference: 2 },
+            ],
+          },
+        },
+        '>',
+      ]);
+    });
 
-  it("parses a snippet with a placeholder that mixes text and tab stop references", () => {
-    expectMatch("$4console.${3:log}('${2:uh $1}', $1);$0", [
-      { index: 4, content: [] },
-      'console.',
-      { index: 3, content: ['log'] },
-      '(\'',
-      {
-        index: 2, content: [
-          'uh ',
-          { index: 1, content: [] }
-        ]
-      },
-      '\', ',
-      { index: 1, content: [] },
-      ');',
-      { index: 0, content: [] }
-    ]);
+    it("parses a snippet with an escaped forward slash in a transform", () => {
+      expectMatch("<${1:p}>$0</${1/(.)\\/(.*)/\\u$1$2/}>", [
+        '<',
+        { index: 1, content: ['p'] },
+        '>',
+        { index: 0, content: [] },
+        '</',
+        {
+          index: 1,
+          transformation: {
+            find: /(.)\/(.*)/,
+            replace: [
+              { modifier: 'u' },
+              { backreference: 1 },
+              { backreference: 2 },
+            ],
+          },
+        },
+        '>',
+      ]);
+    });
+
+    it("parses a snippet with a placeholder that mirrors another tab stop's content", () => {
+      expectMatch("$4console.${3:log}('${2:$1}', $1);$0", [
+        { index: 4, content: [] },
+        'console.',
+        { index: 3, content: ['log'] },
+        '(\'',
+        {
+          index: 2, content: [
+            { index: 1, content: [] }
+          ]
+        },
+        '\', ',
+        { index: 1, content: [] },
+        ');',
+        { index: 0, content: [] }
+      ]);
+    });
+
+    it("parses a snippet with a placeholder that mixes text and tab stop references", () => {
+      expectMatch("$4console.${3:log}('${2:uh $1}', $1);$0", [
+        { index: 4, content: [] },
+        'console.',
+        { index: 3, content: ['log'] },
+        '(\'',
+        {
+          index: 2, content: [
+            'uh ',
+            { index: 1, content: [] }
+          ]
+        },
+        '\', ',
+        { index: 1, content: [] },
+        ');',
+        { index: 0, content: [] }
+      ]);
+    });
   });
 });
